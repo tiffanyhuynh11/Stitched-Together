1d4da390e18c733e50d547330b0dfe4133a186b0
Casey Class <casey.class@sjsu.edu>
2025-05-03 15:46:08 -0700

    db connection method

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,12 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request, url_for, flash, redirect
+import sqlite3
+from werkzeug.exceptions import abort
 from flask_cors import CORS
+
+def get_db_connection():
+    conn = sqlite3.connect('data/database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 CORS(app)  # Allows frontend to communicate with Flask
