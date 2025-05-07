9e0f636d36e9c4e015b7dca83a19e7d0e082a0e7
Casey Class <casey.class@sjsu.edu>
2025-05-06 23:03:18 -0700

    added connection field

--- backend/init_db.py
+++ backend/init_db.py
@@ -8,8 +8,8 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO profiles (name, birthday, relationship, so, notes, gifts) VALUES (?, ?, ?, ?, ?, ?)",
-                 ("Your Name", "", "", "", "", ""))
+cur.execute("INSERT INTO profiles (name, birthday, relationship, connection, so, notes, gifts) VALUES (?, ?, ?, ?, ?, ?)",
+                 ("Your Name", "", "", "", "", "", ""))
 
 
 #cur.execute("INSERT INTO profiles (name, birthday, relationship, so, notes, gifts) VALUES (?, ?, ?, ?, ?, ?)",
